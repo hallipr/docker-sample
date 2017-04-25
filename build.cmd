9a0e58c3c87040a9a5eee788575857ed9e9a7a6b
Patrick Hallisey <pahallis@microsoft.com>
2017-04-24 23:34:51 -0700

    Pull docker files back into root folder

--- build.cmd
+++ build.cmd
@@ -9,9 +9,10 @@ docker create --name build-container build-image
 echo "copy dotnetapp out of build container"
 docker cp build-container:/app/out ./
 
+echo "build image"
+docker build -t dotnetapp-selfcontained .
+
 echo "cleanup"
 docker rm build-container
 docker rmi build-image
-
-echo "cleanup"
-docker build -t dotnetapp-selfcontained .
+rd out /S /Q
--- build.runtime.cmd
+++ build.runtime.cmd
@@ -1,7 +1,7 @@
 @echo off
 
 echo "build dotnetapp via Dockerfile.build"
-docker build -t build-image -f Dockerfile.build .
+docker build -t build-image -f Dockerfile.runtime.build .
 
 echo "start build container"
 docker create --name build-container build-image
@@ -9,9 +9,10 @@ docker create --name build-container build-image
 echo "copy dotnetapp out of build container"
 docker cp build-container:/app/out ./
 
+echo "build image"
+docker build -t dotnetapp -f Dockerfile.runtime .
+
 echo "cleanup"
 docker rm build-container
 docker rmi build-image
-
-echo "cleanup"
-docker build -t dotnetapp .
+rd out /S /Q
