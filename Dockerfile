26319fd4a9e7e064cd3ffcd7c832a9c357718659
Patrick Hallisey <pahallis@microsoft.com>
2017-04-24 21:39:25 -0700

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM microsoft/nanoserver:10.0.14393.1066
+WORKDIR /app
+COPY out .
+ENTRYPOINT ["dotnetapp.exe"]
---
+++ build.cmd
@@ -0,0 +1,16 @@
+@echo off
+
+echo "build dotnetapp via Dockerfile.build"
+docker build -t build-image -f Dockerfile.build .
+
+echo "start build container"
+docker create --name build-container build-image
+
+echo "copy dotnetapp out of build container"
+docker cp build-container:/app/out ./
+
+echo "cleanup"
+docker rm build-container
+
+echo "cleanup"
+docker build -t dotnetapp-selfcontained .
